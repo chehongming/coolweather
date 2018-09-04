c3e162c70c7d7f976ae3cebde6294f4123597c87
chehongming <chm1823015355@163.com>
2018-09-04 12:51:44 +0800

    Second commit

---
+++ app/src/main/java/com/example/chm/coolweather/db/County.java
@@ -0,0 +1,38 @@
+package com.example.chm.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by chm on 18-9-4.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getCountyName() {
+        return countyName;
+    }
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+    public String getWeatherId() {
+        return weatherId;
+    }
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+    public int getCityId() {
+        return cityId;
+    }
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
